3c5a7ffdf826c9e72ccca810f44bc87c615fc23b
Tu Nombre <usuario@ejemplo.com>
2025-09-24 19:28:07 -0300

    feat: Inicializar proyecto de extensión de VS Code

---
+++ src/extension.ts
@@ -0,0 +1,14 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	console.log('Congratulations, your extension "codigo-duplicado-detector" is now active!');
+
+	let disposable = vscode.commands.registerCommand('codigo-duplicado-detector.helloWorld', () => {
+		vscode.window.showInformationMessage('Hello World from Codigo Duplicado Detector!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}

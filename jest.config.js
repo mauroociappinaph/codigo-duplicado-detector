2f033ae4f21d7e71c9f5609c5ee3ce8a9b040f51
Tu Nombre <usuario@ejemplo.com>
2025-09-25 13:15:20 -0300

    feat(detection): Install jscpd, ts-morph and set up DetectionService

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,7 @@ module.exports = {
   testMatch: ['<rootDir>/src/**/*.test.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   roots: ['<rootDir>/src'],
+  moduleNameMapper: {
+    '^vscode$': '<rootDir>/src/test/__mocks__/vscode.ts',
+  },
 };
